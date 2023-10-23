cb3b79f69417a9cf0cab155914d1d8d13e8c6d25
Ladbaby <1390265970@qq.com>
2023-10-23 10:57:56 +0800

    Add options and README

---
+++ README.md
@@ -0,0 +1,61 @@
+# Profile Server
+
+A simple profile server supporting modified response header (non production) 
+
+## Motivation
+
+I need to serve a file with some additioinal http response header field needed to be evaluated from command line utilities. 
+
+Since Nginx seems to be not capable of it, I wrote this simple server instead.
+
+## Features
+
+- Send a file when accessed via the specified route path
+- The above file will be sent with a modified header, including additional fields:
+
+    - `subscription-userinfo`
+
+        e.g., 
+
+        ```
+        subscription-userinfo: upload=455727941; download=6174315083; total=1073741824000; expire=1671815872
+        ```
+    
+    - `content-disposition`
+
+        e.g.,
+
+        ```
+        attachment; filename*=UTF-8''profile_name
+        ```
+
+        > the profile name supports UTF-8, which means it can even contain emoji
+
+## Installation
+
+Clone this repo and install the required libraries:
+
+```shell
+pip install flask
+```
+
+Make sure you have `vnstat` installed to get the traffic data, for instance Debian:
+
+```shell
+sudo apt install vnstat
+```
+
+## Usage
+
+See the command line options:
+
+```shell
+python3 main.py --help
+```
+
+minimal example:
+
+```shell
+python3 main.py --file-path /path/to/file
+```
+    
--- main.py
+++ main.py
@@ -6,53 +6,53 @@ from urllib.parse import quote
 
 app = Flask(__name__)
 
-upload = 0
-download = 0
-total = 1099511627776
-
-
 def get_args():
     parser = argparse.ArgumentParser(description='Python file server with dynamic headers')
     parser.add_argument('--file-path', type=str, required=True, help='Path to the file to serve')
-    parser.add_argument('--file-name', type=str, help='Custom display name of the profile')
-    parser.add_argument('--route-path', type=str, required=True, help='URL route path')
-    parser.add_argument('--port', type=int, default=18010, help='Port number for the server (default: 18010)')
+    parser.add_argument('--file-name', type=str, help='Custom display name of the profile (support UTF-8)')
+    parser.add_argument('--route-path', type=str, default='', help='URL route path')
+    parser.add_argument('--address', type=str, default='127.0.0.1', help='Address the server will listen on')
+    parser.add_argument('--port', type=int, default=18010, help='Port number for the server')
+    parser.add_argument('--total-bandwidth', type=int, default=1099511627776, help='Bandwidth limit per month in byte')
+    parser.add_argument('--expire', type=int, default=None, help='Expire time of the profile in seconds from 00:00:00 UTC, January 1, 1970')
+    parser.add_argument('--update-interval', type=int, help='Profile update interval in hours')
+    parser.add_argument('--web-url', type=str, help='the URL to be opened via Open web page option in context menu')
     return parser.parse_args()
 
 args = get_args()
 
 # Function to get dynamic data from vnstat
 def get_vnstat_data():
-    global upload
-    global download
     try:
         result = json.loads(subprocess.check_output(['vnstat', '--json']).decode('utf-8'))
         upload = result["interfaces"][0]["traffic"]["month"][0]["tx"]
+        # note: Vultr only calculate outbound traffic, thus rx not used
         # download = result["interfaces"][0]["traffic"]["month"][0]["rx"]
-        return True
+        download = 0
+        return upload, download
     except subprocess.CalledProcessError as e:
-        return None
+        return None, None
 
 @app.route(f'/{args.route_path}')
 def serve_file():
     global args
-    global upload
-    global download
-    global total
-    file_path = args.file_path
-    file_name = args.file_name
     
     # Get dynamic data from vnstat
-    get_vnstat_data()
+    upload, download = get_vnstat_data()
     
     # Modify the response headers
-    response = send_file(file_path)
+    response = send_file(args.file_path)
 
-    response.headers['Subscription-Userinfo'] = f"upload={upload}; download={download}; total={total}; expire="
-    if file_name:
-        response.headers['content-disposition'] = f"attachment; filename*=UTF-8''{quote(file_name)}"
+    if (upload and download) is not None:
+        response.headers['subscription-userinfo'] = f"upload={upload}; download={download}; total={args.total_bandwidth}; expire={args.expire or ''}"
+        if args.file_name:
+            response.headers['content-disposition'] = f"attachment; filename*=UTF-8''{quote(args.file_name)}"
+        if args.update_interval:
+            response.headers['profile-update-interval'] = f"{args.update_interval}"
+        if args.web_url:
+            response.headers['profile-web-page-url'] = args.web_url
 
     return response
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=args.port)
+    app.run(host=args.address, port=args.port)
